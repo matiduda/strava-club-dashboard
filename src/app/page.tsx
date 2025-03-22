a7d2bb39165d0da01c22c492f2c530e19f26760b
Michał Wrona <mwrona141@gmail.com>
2025-03-22 03:06:31 +0100

    feat: add text with info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,10 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center h-screen  ">
       <Image src={logo} alt="Team Wrona Logo" width={150} />
       <h1 className="text-3xl font-bold">Dashboard for Strava Club</h1>
+      <p className="mt-2 text-gray-600">
+        Aby korzystać z aplikacji i zobaczyć dane klubu, zaloguj się przez
+        Strava.
+      </p>
       <LoginByStravaButton />
     </div>
   );
