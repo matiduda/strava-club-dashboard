46aedb3e4c6707090b02e2728d3c7110e12cf77c
Michał Wrona <mwrona141@gmail.com>
2025-03-22 02:52:34 +0100

    feat: add button for login on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import logo from "../../public/imgs/team_wrona_logo.png";
+import LoginByStravaButton from "@/components/atoms/LoginByStravaButton";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen  ">
       <Image src={logo} alt="Team Wrona Logo" width={150} />
       <h1 className="text-3xl font-bold">Dashboard for Strava Club</h1>
+      <LoginByStravaButton />
     </div>
   );
 }
